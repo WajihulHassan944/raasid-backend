bc094be03338909aee0c75611a735e9813a30a00
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-07-01 13:38:32 +0500

    updated code

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const schema = new mongoose.Schema(
     },
     role: {
     type: [String],
-    enum: ["user", "admin"],
+    enum: ["user", "admin", "subadmin"],
     default: ["user"],
   },
   },
